0240f48569fe1fa8b8fcf8b268a27d1fef366671
abkgit <elite.abhishek@gmail.com>
2017-01-05 04:54:21 +0530

    1.1 UI Coded, Initial File Structure Ready.

---
+++ app/src/main/java/com/example/android/inventorymanagement/ProductCursorAdapter.java
@@ -0,0 +1,8 @@
+package com.example.android.inventorymanagement;
+
+/**
+ * Created by Colossus on 05-01-2017.
+ */
+
+public class ProductCursorAdapter {
+}
---
+++ app/src/main/java/com/example/android/inventorymanagement/data/ProductContract.java
@@ -0,0 +1,4 @@
+package com.example.android.inventorymanagement.data;
+
+public class ProductContract {
+}
