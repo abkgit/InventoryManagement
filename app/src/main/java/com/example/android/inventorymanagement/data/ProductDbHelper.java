82cb5c11947813ca8b4469ebe4848099b4c79d45
abkgit <elite.abhishek@gmail.com>
2017-01-05 16:26:39 +0530

    1.4 ProductProvider query(), insert() and update() methods coded.

--- app/src/main/java/com/example/android/inventorymanagement/data/ProductDbHelper.java
+++ app/src/main/java/com/example/android/inventorymanagement/data/ProductDbHelper.java
@@ -41,7 +41,7 @@ public class ProductDbHelper extends SQLiteOpenHelper {
         String SQL_CREATE_PRODUCTS_TABLE = "CREATE TABLE " + ProductEntry.TABLE_NAME + " ("
                 + ProductEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
                 + ProductEntry.COLUMN_PRODUCT_NAME + " TEXT NOT NULL, "
-                + ProductEntry.COLUMN_PRODUCT_PRICE + " REAL NOT NULL, "
+                + ProductEntry.COLUMN_PRODUCT_PRICE + " REAL DEFAULT 0.00, "
                 + ProductEntry.COLUMN_PRODUCT_QUANTITY + " INTEGER DEFAULT 0, "
                 + ProductEntry.COLUMN_PRODUCT_SUPPLIER + " TEXT NOT NULL, "
                 + ProductEntry.COLUMN_PRODUCT_PHOTO + " TEXT, ";
