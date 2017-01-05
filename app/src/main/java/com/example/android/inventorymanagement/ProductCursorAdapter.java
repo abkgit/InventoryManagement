5a65631d745f04d5ca3a847c48413b4fff723ad8
abkgit <elite.abhishek@gmail.com>
2017-01-05 17:26:53 +0530

    1.5 ProductCursorAdapter coded.

--- app/src/main/java/com/example/android/inventorymanagement/ProductCursorAdapter.java
+++ app/src/main/java/com/example/android/inventorymanagement/ProductCursorAdapter.java
@@ -1,8 +1,78 @@
 package com.example.android.inventorymanagement;
 
-/**
- * Created by Colossus on 05-01-2017.
- */
+import android.content.Context;
+import android.database.Cursor;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+import android.widget.TextView;
 
-public class ProductCursorAdapter {
+import com.example.android.inventorymanagement.data.ProductContract.ProductEntry;
+
+
+/**
+ * {@link ProductCursorAdapter} is an adapter for a list view
+ * that uses a {@link Cursor} of product data as its data source. This adapter knows
+ * how to create list items for each row of product data in the {@link Cursor}.
+ */
+public class ProductCursorAdapter extends CursorAdapter {
+
+    /**
+     * Constructs a new {@link ProductCursorAdapter}.
+     *
+     * @param context The context
+     * @param c       The cursor from which to get the data.
+     */
+    public ProductCursorAdapter(Context context, Cursor c) {
+        super(context, c, 0 /* flags */);
+    }
+
+    /**
+     * Makes a new blank list item view. No data is set (or bound) to the views yet.
+     *
+     * @param context app context
+     * @param cursor  The cursor from which to get the data. The cursor is already
+     *                moved to the correct position.
+     * @param parent  The parent to which the new view is attached to
+     * @return the newly created list item view.
+     */
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        // Inflate a list item view using the layout specified in list_item.xml
+        return LayoutInflater.from(context).inflate(R.layout.list_item, parent, false);
+    }
+
+    /**
+     * This method binds the product data (in the current row pointed to by cursor) to the given
+     * list item layout. For example, the name for the current product can be set on the name
+     * TextView in the list item layout.
+     *
+     * @param view    Existing view, returned earlier by newView() method
+     * @param context app context
+     * @param cursor  The cursor from which to get the data. The cursor is already moved to the
+     *                correct row.
+     */
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+        // Find individual views that we want to modify in the list item layout
+        TextView nameTextView = (TextView) view.findViewById(R.id.list_product_name);
+        TextView quantityTextView = (TextView) view.findViewById(R.id.list_product_quantity);
+        TextView priceTextView = (TextView) view.findViewById(R.id.list_product_price);
+
+        // Find the columns of pet attributes that we're interested in
+        int nameColumnIndex = cursor.getColumnIndex(ProductEntry.COLUMN_PRODUCT_NAME);
+        int quantityColumnIndex = cursor.getColumnIndex(ProductEntry.COLUMN_PRODUCT_QUANTITY);
+        int priceColumnIndex = cursor.getColumnIndex(ProductEntry.COLUMN_PRODUCT_PRICE);
+
+        // Read the pet attributes from the Cursor for the current pet
+        String productName = cursor.getString(nameColumnIndex);
+        String productQuantity = cursor.getString(quantityColumnIndex);
+        String productPrice = cursor.getString(priceColumnIndex);
+
+        // Update the TextViews with the attributes for the current pet
+        nameTextView.setText(productName);
+        quantityTextView.setText(productQuantity);
+        priceTextView.setText(productPrice);
+    }
 }
