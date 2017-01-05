4a2eda6e76785c68c569bf05cf8ce7de25647592
abkgit <elite.abhishek@gmail.com>
2017-01-05 15:15:39 +0530

    1.2 ProductContract class coded.

--- app/src/main/java/com/example/android/inventorymanagement/data/ProductContract.java
+++ app/src/main/java/com/example/android/inventorymanagement/data/ProductContract.java
@@ -1,4 +1,109 @@
 package com.example.android.inventorymanagement.data;
 
+import android.content.ContentResolver;
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+/**
+ * API Contract for the Inventory Management app.
+ */
 public class ProductContract {
+
+    // To prevent someone from accidentally instantiating the contract class,
+    // give it an empty constructor.
+    public ProductContract() {
+    }
+
+    /**
+     * The "Content authority" is a name for the entire content provider, similar to the
+     * relationship between a domain name and its website.  A convenient string to use for the
+     * content authority is the package name for the app, which is guaranteed to be unique on the
+     * device.
+     */
+    public static final String CONTENT_AUTHORITY = "com.example.android.inventorymanagement";
+
+    /**
+     * Use CONTENT_AUTHORITY to create the base of all URI's which apps will use to contact
+     * the content provider.
+     */
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    /**
+     * Possible path (appended to base content URI for possible URI's)
+     * For instance, content://com.example.android.inventorymanagement/inventory/ is a valid path
+     * for looking at product data. content://com.example.android.inventorymanagement/staff/ will
+     * fail, as the ContentProvider hasn't been given any information on what to do with "staff".
+     */
+    public static final String PATH_INVENTORY = "inventory";
+
+    /**
+     * Inner class that defines constant values for the inventory database table.
+     * Each entry in the table represents a single product.
+     */
+    public static final class InventoryEntry implements BaseColumns {
+
+        /**
+         * The content URI to access the product data in the provider
+         */
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_INVENTORY);
+
+        /**
+         * The MIME type of the {@link #CONTENT_URI} for a list of products.
+         */
+        public static final String CONTENT_LIST_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_INVENTORY;
+
+        /**
+         * The MIME type of the {@link #CONTENT_URI} for a single product.
+         */
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_INVENTORY;
+
+        /**
+         * Name of database table for products
+         */
+        public final static String TABLE_NAME = "inventory";
+
+        /**
+         * Unique ID number for the product (only for use in the database table).
+         * <p>
+         * Type: INTEGER
+         */
+        public final static String _ID = BaseColumns._ID;
+
+        /**
+         * Name of the product.
+         * <p>
+         * Type: TEXT
+         */
+        public final static String COLUMN_PRODUCT_NAME = "name";
+
+        /**
+         * Price of the product.
+         * <p>
+         * Type: REAL
+         */
+        public final static String COLUMN_PRODUCT_PRICE = "price";
+
+        /**
+         * Available quantity of the product.
+         * <p>
+         * Type: INTEGER
+         */
+        public final static String COLUMN_PRODUCT_QUANTITY = "quantity";
+
+        /**
+         * Name of the product supplier.
+         * <p>
+         * Type: TEXT
+         */
+        public final static String COLUMN_PRODUCT_SUPPLIER = "supplier";
+
+        /**
+         * URI of the product photo.
+         * <p>
+         * Type: TEXT
+         */
+        public final static String COLUMN_PRODUCT_PHOTO = "photo";
+    }
 }
